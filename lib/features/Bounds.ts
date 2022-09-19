5a2a689f65c6e93d8ed9d445b6c40942998a37e2
Kevin Gilland <kgilland@caci.com>
2022-09-19 12:12:19 -0600

    chore: Partial import of features classes.  Still not building.

---
+++ lib/features/Bounds.ts
@@ -0,0 +1,704 @@
+import { GeometryEnvelope } from '@ngageoint/simple-features-js';
+import { Unit } from './Unit';
+import { List } from 'tstl';
+import { Point } from './Point';
+import { Line } from './Line';
+
+/**
+ * Grid Bounds
+ * 
+ * @author osbornb
+ */
+export class Bounds extends GeometryEnvelope {
+
+	/**
+	 * Serial Version UID
+	 */
+	private static readonly serialVersionUID = 1;
+
+	/**
+	 * Unit
+	 */
+	private unit?: Unit = undefined;
+
+	/**
+	 * Create bounds
+	 * 
+	 * @param minLongitude
+	 *            min longitude
+	 * @param minLatitude
+	 *            min latitude
+	 * @param maxLongitude
+	 *            max longitude
+	 * @param maxLatitude
+	 *            max latitude
+	 * @param unit
+	 *            unit
+	 * @return bounds
+	 */
+	public static bounds(minLongitude: number, minLatitude: number,
+		maxLongitude: number, maxLatitude: number, unit: Unit): Bounds {
+		return new Bounds(minLongitude, minLatitude, maxLongitude, maxLatitude,
+			unit);
+	}
+
+	/**
+	 * Create bounds in degrees
+	 * 
+	 * @param minLongitude
+	 *            min longitude
+	 * @param minLatitude
+	 *            min latitude
+	 * @param maxLongitude
+	 *            max longitude
+	 * @param maxLatitude
+	 *            max latitude
+	 * @return bounds
+	 */
+	public static degrees(minLongitude: number, minLatitude: number,
+		maxLongitude: number, maxLatitude: number): Bounds {
+		return this.bounds(minLongitude, minLatitude, maxLongitude, maxLatitude,
+			Unit.DEGREE);
+	}
+
+	/**
+	 * Create bounds in meters
+	 * 
+	 * @param minLongitude
+	 *            min longitude
+	 * @param minLatitude
+	 *            min latitude
+	 * @param maxLongitude
+	 *            max longitude
+	 * @param maxLatitude
+	 *            max latitude
+	 * @return bounds
+	 */
+	public static meters(minLongitude: number, minLatitude: number,
+		maxLongitude: number, maxLatitude: number): Bounds {
+		return this.bounds(minLongitude, minLatitude, maxLongitude, maxLatitude,
+			Unit.METER);
+	}
+
+	/**
+	 * Create bounds
+	 * 
+	 * @param southwest
+	 *            southwest corner
+	 * @param northeast
+	 *            northeast corner
+	 * @return bounds
+	 */
+	public static bounds(southwest: Point, northeast: Point): Bounds {
+		return new Bounds(southwest, northeast);
+	}
+
+	/**
+	 * Copy bounds
+	 * 
+	 * @param bounds
+	 *            bounds to copy
+	 * @return bounds
+	 */
+	public static bounds(bounds: Bounds): Bounds {
+		return new Bounds(bounds);
+	}
+
+	/**
+	 * Create bounds
+	 * 
+	 * @param envelope
+	 *            geometry envelope
+	 * @param unit
+	 *            unit
+	 * @return bounds
+	 */
+	public static bounds(envelope: GeometryEnvelope, unit: Unit): Bounds {
+		return new Bounds(envelope, unit);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param minLongitude
+	 *            min longitude
+	 * @param minLatitude
+	 *            min latitude
+	 * @param maxLongitude
+	 *            max longitude
+	 * @param maxLatitude
+	 *            max latitude
+	 */
+	public Bounds(double minLongitude, double minLatitude, double maxLongitude,
+		double maxLatitude) {
+		this(minLongitude, minLatitude, maxLongitude, maxLatitude, Unit.DEGREE);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param minLongitude
+	 *            min longitude
+	 * @param minLatitude
+	 *            min latitude
+	 * @param maxLongitude
+	 *            max longitude
+	 * @param maxLatitude
+	 *            max latitude
+	 * @param unit
+	 *            unit
+	 */
+	public Bounds(double minLongitude, double minLatitude, double maxLongitude,
+		double maxLatitude, Unit unit) {
+		super(minLongitude, minLatitude, maxLongitude, maxLatitude);
+		this.unit = unit;
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param southwest
+	 *            southwest corner
+	 * @param northeast
+	 *            northeast corner
+	 */
+	public Bounds(Point southwest, Point northeast) {
+		this(southwest.getLongitude(), southwest.getLatitude(),
+			northeast.getLongitude(), northeast.getLatitude(),
+			southwest.getUnit());
+
+		if (!isUnit(northeast.getUnit())) {
+			throw new IllegalArgumentException(
+				"Points are in different units. southwest: " + unit
+				+ ", northeast: " + northeast.getUnit());
+		}
+	}
+
+	/**
+	 * Copy constructor
+	 * 
+	 * @param bounds
+	 *            bounds to copy
+	 */
+	public Bounds(Bounds bounds) {
+		this(bounds, bounds.unit);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param envelope
+	 *            geometry envelope
+	 * @param unit
+	 *            unit
+	 */
+	public Bounds(GeometryEnvelope envelope, Unit unit) {
+		super(envelope);
+		this.unit = unit;
+	}
+
+	/**
+	 * Get the min longitude
+	 * 
+	 * @return min longitude
+	 */
+	public getMinLongitude(): number {
+		return super.minX;
+	}
+
+	/**
+	 * Set the min longitude
+	 * 
+	 * @param minLongitude
+	 *            min longitude
+	 */
+	public setMinLongitude(minLongitude: number): void {
+		super.minX = minLongitude;
+	}
+
+	/**
+	 * Get the min latitude
+	 * 
+	 * @return min latitude
+	 */
+	public getMinLatitude(): number {
+		return super.minY;
+	}
+
+	/**
+	 * Set the min latitude
+	 * 
+	 * @param minLatitude
+	 *            min latitude
+	 */
+	public setMinLatitude(minLatitude: number): void {
+		super.minY = minLatitude;
+	}
+
+	/**
+	 * Get the max longitude
+	 * 
+	 * @return max longitude
+	 */
+	public getMaxLongitude(): number {
+		return super.maxX;
+	}
+
+	/**
+	 * Set the max longitude
+	 * 
+	 * @param maxLongitude
+	 *            max longitude
+	 */
+	public setMaxLongitude(maxLongitude: number): void {
+		super.maxX = maxLongitude;
+	}
+
+	/**
+	 * Get the max latitude
+	 * 
+	 * @return max latitude
+	 */
+	public getMaxLatitude(): number {
+		return super.maxY;;
+	}
+
+	/**
+	 * Set the max latitude
+	 * 
+	 * @param maxLatitude
+	 *            max latitude
+	 */
+	public setMaxLatitude(maxLatitude: number): void {
+		super.maxY = maxLatitude;
+	}
+
+	/**
+	 * Get the western longitude
+	 * 
+	 * @return western longitude
+	 */
+	public getWest(): number {
+		return this.getMinLongitude();
+	}
+
+	/**
+	 * Set the western longitude
+	 * 
+	 * @param west
+	 *            western longitude
+	 */
+	public setWest(west: number): void {
+		this.setMinLongitude(west);
+	}
+
+	/**
+	 * Get the southern latitude
+	 * 
+	 * @return southern latitude
+	 */
+	public getSouth(): number {
+		return this.getMinLatitude();
+	}
+
+	/**
+	 * Set the southern latitude
+	 * 
+	 * @param south
+	 *            southern latitude
+	 */
+	public setSouth(south: number): void {
+		this.setMinLatitude(south);
+	}
+
+	/**
+	 * Get the eastern longitude
+	 * 
+	 * @return eastern longitude
+	 */
+	public getEast(): number {
+		return this.getMaxLongitude();
+	}
+
+	/**
+	 * Set the eastern longitude
+	 * 
+	 * @param east
+	 *            eastern longitude
+	 */
+	public setEast(east: number): void {
+		this.setMaxLongitude(east);
+	}
+
+	/**
+	 * Get the northern latitude
+	 * 
+	 * @return northern latitude
+	 */
+	public getNorth(): number {
+		return this.getMaxLatitude();
+	}
+
+	/**
+	 * Set the northern latitude
+	 * 
+	 * @param north
+	 *            northern latitude
+	 */
+	public setNorth(north: number): void {
+		this.setMaxLatitude(north);
+	}
+
+	/**
+	 * Get the unit
+	 * 
+	 * @return unit
+	 */
+	public getUnit(): Unit | undefined {
+		return this.unit;
+	}
+
+	/**
+	 * Set the unit
+	 * 
+	 * @param unit
+	 *            unit
+	 */
+	public setUnit(unit: Unit): void {
+		this.unit = unit;
+	}
+
+	/**
+	 * Is in the provided unit type
+	 * 
+	 * @param unit
+	 *            unit
+	 * @return true if in the unit
+	 */
+	public isUnit(unit: Unit): boolean {
+		return this.unit == unit;
+	}
+
+	/**
+	 * Are bounds in degrees
+	 * 
+	 * @return true if degrees
+	 */
+	public isDegrees(): boolean {
+		return this.isUnit(Unit.DEGREE);
+	}
+
+	/**
+	 * Are bounds in meters
+	 * 
+	 * @return true if meters
+	 */
+	public isMeters(): boolean {
+		return this.isUnit(Unit.METER);
+	}
+
+	/**
+	 * Convert to the unit
+	 * 
+	 * @param unit
+	 *            unit
+	 * @return bounds in units, same bounds if equal units
+	 */
+	public toUnit(unit: Unit): Bounds {
+		let bounds: Bounds | undefined = undefined;
+		if (this.isUnit(unit)) {
+			bounds = this;
+		} else {
+			const southwest = this.getSouthwest().toUnit(unit);
+			const northeast = this.getNortheast().toUnit(unit);
+			bounds = new Bounds(southwest, northeast);
+		}
+		return bounds;
+	}
+
+	/**
+	 * Convert to degrees
+	 * 
+	 * @return bounds in degrees, same bounds if already in degrees
+	 */
+	public toDegrees(): Bounds {
+		return this.toUnit(Unit.DEGREE);
+	}
+
+	/**
+	 * Convert to meters
+	 * 
+	 * @return bounds in meters, same bounds if already in meters
+	 */
+	public toMeters(): Bounds {
+		return this.toUnit(Unit.METER);
+	}
+
+	/**
+	 * Get the centroid longitude
+	 * 
+	 * @return centroid longitude
+	 */
+	public getCentroidLongitude(): number {
+		return this.getMidX();
+	}
+
+	/**
+	 * Get the centroid latitude
+	 * 
+	 * @return centroid latitude
+	 */
+	public getCentroidLatitude(): number {
+		let centerLatitude: number;
+		if (this.unit == Unit.DEGREE) {
+			centerLatitude = this.getCentroid().getLatitude();
+		} else {
+			centerLatitude = this.getMidY();
+		}
+		return centerLatitude;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public getCentroid(): Point {
+		let point: Point | undefined = undefined;
+		if (this.unit == Unit.DEGREE) {
+			point = this.toMeters().getCentroid().toDegrees();
+		} else {
+			point = Point.point(super.centroid, this.unit);
+		}
+		return point;
+	}
+
+	/**
+	 * Get the width
+	 * 
+	 * @return width
+	 */
+	public getWidth(): number {
+		return super.xRange;
+	}
+
+	/**
+	 * Get the height
+	 * 
+	 * @return height
+	 */
+	public getHeight(): number {
+		return super.yRange;
+	}
+
+	/**
+	 * Get the southwest coordinate
+	 * 
+	 * @return southwest coordinate
+	 */
+	public getSouthwest(): Point {
+		return Point.point(this.getMinLongitude(), this.getMinLatitude(), this.unit);
+	}
+
+	/**
+	 * Get the northwest coordinate
+	 * 
+	 * @return northwest coordinate
+	 */
+	public getNorthwest(): Point {
+		return Point.point(this.getMinLongitude(), this.getMaxLatitude(), this.unit);
+	}
+
+	/**
+	 * Get the southeast coordinate
+	 * 
+	 * @return southeast coordinate
+	 */
+	public getSoutheast(): Point {
+		return Point.point(this.getMaxLongitude(), this.getMinLatitude(), this.unit);
+	}
+
+	/**
+	 * Get the northeast coordinate
+	 * 
+	 * @return northeast coordinate
+	 */
+	public getNortheast(): Point {
+		return Point.point(this.getMaxLongitude(), this.getMaxLatitude(), this.unit);
+	}
+
+	/**
+	 * Create a new bounds as the overlapping between this bounds and the
+	 * provided
+	 * 
+	 * @param bounds
+	 *            bounds
+	 * @return overlap bounds
+	 */
+	public overlap(bounds: Bounds): Bounds | undefined {
+
+		let unionOverlap: Bounds | undefined = undefined;
+
+		let overlapEnvelope = super.overlap(bounds.toUnit(this.unit), true);
+		if (overlapEnvelope != null) {
+			unionOverlap = new Bounds(overlapEnvelope, this.unit);
+		}
+
+		return unionOverlap;
+	}
+
+	/**
+	 * Create a new bounds as the union between this bounds and the provided
+	 * 
+	 * @param bounds
+	 *            bounds
+	 * @return union bounds
+	 */
+	public union(bounds: Bounds): Bounds | undefined {
+
+		let unionBounds: Bounds | undefined = undefined;
+
+		let unionEnvelope = super.union(bounds.toUnit(this.unit));
+		if (unionEnvelope != null) {
+			unionBounds = new Bounds(unionEnvelope, this.unit);
+		}
+
+		return unionBounds;
+	}
+
+	/**
+	 * Get the western line
+	 * 
+	 * @return west line
+	 */
+	public getWestLine(): Line {
+		return Line.line(this.getNorthwest(), this.getSouthwest());
+	}
+
+	/**
+	 * Get the southern line
+	 * 
+	 * @return south line
+	 */
+	public getSouthLine(): Line {
+		return Line.line(this.getSouthwest(), this.getSoutheast());
+	}
+
+	/**
+	 * Get the eastern line
+	 * 
+	 * @return east line
+	 */
+	public getEastLine(): Line {
+		return Line.line(this.getSoutheast(), this.getNortheast());
+	}
+
+	/**
+	 * Get the northern line
+	 * 
+	 * @return north line
+	 */
+	public getNorthLine(): Line {
+		return Line.line(this.getNortheast(), this.getNorthwest());
+	}
+
+	/**
+	 * Convert the bounds to be precision accurate minimally containing the
+	 * bounds. Each bound is equal to or larger by the precision degree amount.
+	 * 
+	 * @param precision
+	 *            precision in degrees
+	 * @return precision bounds
+	 */
+	public toPrecision(precision: number): Bounds {
+
+		const bounds = this.toDegrees();
+
+		const minLon = GridUtils.precisionBefore(bounds.getMinLongitude(),
+			precision);
+		const minLat = GridUtils.precisionBefore(bounds.getMinLatitude(),
+			precision);
+		const maxLon = GridUtils.precisionAfter(bounds.getMaxLongitude(),
+			precision);
+		const maxLat = GridUtils.precisionAfter(bounds.getMaxLatitude(),
+			precision);
+
+		return Bounds.degrees(minLon, minLat, maxLon, maxLat);
+	}
+
+	/**
+	 * Get the pixel range where the bounds fit into the tile
+	 * 
+	 * @param tile
+	 *            tile
+	 * @return pixel range
+	 */
+	public getPixelRange(tile: GridTile): PixelRange {
+		return this.getPixelRange(tile.getWidth(), tile.getHeight(),
+			tile.getBounds());
+	}
+
+	/**
+	 * Get the pixel range where the bounds fit into the provided bounds
+	 * 
+	 * @param width
+	 *            width
+	 * @param height
+	 *            height
+	 * @param bounds
+	 *            bounds
+	 * @return pixel range
+	 */
+	public getPixelRange(width: number, height: number, bounds: Bounds): PixelRange {
+		bounds = bounds.toMeters();
+		const topLeft = GridUtils.getPixel(width, height, bounds,
+			this.getNorthwest());
+		const bottomRight = GridUtils.getPixel(width, height, bounds,
+			this.getSoutheast());
+		return new PixelRange(topLeft, bottomRight);
+	}
+
+	/**
+	 * Get the four line bounds in meters
+	 * 
+	 * @return lines
+	 */
+	public getLines(): List<Line> {
+
+		const southwest = this.getSouthwest();
+		const northwest = this.getNorthwest();
+		const northeast = this.getNortheast();
+		const southeast = this.getSoutheast();
+
+		const lines = new List<Line>();
+		lines.push(Line.line(southwest, northwest));
+		lines.push(Line.line(northwest, northeast));
+		lines.push(Line.line(northeast, southeast));
+		lines.push(Line.line(southeast, southwest));
+
+		return lines;
+	}
+
+	/**
+	 * Copy the bounds
+	 * 
+	 * @return bounds copy
+	 */
+	public copy(): Bounds {
+		return new Bounds(this);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public equals(other: Bounds): boolean {
+		if (this == other)
+			return true;
+		if (!super.equals(other as GeometryEnvelope))
+			return false;
+		if (!(other instanceof Bounds))
+			return false;
+		if (this.unit != other.unit)
+			return false;
+		return true;
+	}
+
+}
---
+++ lib/features/Point.ts
@@ -0,0 +1,315 @@
+import { Point as MilPoint } from "@ngageoint/simple-features-js"
+import { Unit } from "./Unit";
+
+/**
+ * Point
+ * 
+ * @author osbornb
+ */
+export class Point extends MilPoint {
+
+	/**
+	 * Serial Version UID
+	 */
+	private static readonly serialVersionUID = 1;
+
+	/**
+	 * Unit
+	 */
+	private unit?: Unit = undefined;
+
+	/**
+	 * Create a point
+	 * 
+	 * @param longitude
+	 *            longitude
+	 * @param latitude
+	 *            latitude
+	 * @param unit
+	 *            unit; if null, it will use the default degree unit
+	 * @return point
+	 */
+	public static point(longitude: number, latitude: number, unit?: Unit): Point {
+		let point: Point;
+		if (unit) {
+			point = new Point(longitude, latitude, unit);
+		} else {
+			point = this.degrees(longitude, latitude);
+		}
+		return point;
+	}
+
+	/**
+	 * Create a point in degrees
+	 * 
+	 * @param longitude
+	 *            longitude in degrees
+	 * @param latitude
+	 *            latitude in degrees
+	 * @return point in degrees
+	 */
+	public static degrees(longitude: number, latitude: number): Point {
+		return Point.point(longitude, latitude, Unit.DEGREE);
+	}
+
+	/**
+	 * Create a point in meters
+	 * 
+	 * @param longitude
+	 *            longitude in meters
+	 * @param latitude
+	 *            latitude in meters
+	 * @return point in meters
+	 */
+	public static meters(longitude: number, latitude: number): Point {
+		return Point.point(longitude, latitude, Unit.METER);
+	}
+
+	/**
+	 * Create a point from a coordinate in a unit to another unit
+	 * 
+	 * @param fromUnit
+	 *            unit of provided coordinate
+	 * @param longitude
+	 *            longitude
+	 * @param latitude
+	 *            latitude
+	 * @param toUnit
+	 *            desired unit
+	 * @return point in unit
+	 */
+	public static toUnit(fromUnit: Unit, longitude: number, latitude: number,
+		toUnit: Unit): Point {
+		return GridUtils.toUnit(fromUnit, longitude, latitude, toUnit);
+	}
+
+	/**
+	 * Create a point from a coordinate in an opposite unit to another unit
+	 * 
+	 * @param longitude
+	 *            longitude
+	 * @param latitude
+	 *            latitude
+	 * @param unit
+	 *            desired unit
+	 * @return point in unit
+	 */
+	public static toUnitInverse(longitude: number, latitude: number, unit: Unit): Point {
+		return GridUtils.toUnit(longitude, latitude, unit);
+	}
+
+	/**
+	 * Create a point converting the degrees coordinate to meters
+	 * 
+	 * @param longitude
+	 *            longitude in degrees
+	 * @param latitude
+	 *            latitude in degrees
+	 * @return point in meters
+	 */
+	public static degreesToMeters(longitude: number, latitude: number): Point {
+		return Point.toUnit(Unit.DEGREE, longitude, latitude, Unit.METER);
+	}
+
+	/**
+	 * Create a point converting the meters coordinate to degrees
+	 * 
+	 * @param longitude
+	 *            longitude in meters
+	 * @param latitude
+	 *            latitude in meters
+	 * @return point in degrees
+	 */
+	public static metersToDegrees(longitude: number, latitude: number): Point {
+		return Point.toUnit(Unit.METER, longitude, latitude, Unit.DEGREE);
+	}
+
+	/**
+	 * Create a point
+	 * 
+	 * @param point
+	 *            point to copy
+	 * @param unit
+	 *            unit
+	 * @return point
+	 */
+	public static pointFromPoint(point: MilPoint, unit?: Unit): Point {
+		let newPoint = new Point(point);
+		newPoint.unit = unit;
+		return newPoint;
+	}
+
+	/**
+	 * Get the longitude
+	 * 
+	 * @return longitude
+	 */
+	public getLongitude(): number {
+		return super.x;
+	}
+
+	/**
+	 * Set the longitude
+	 * 
+	 * @param longitude
+	 *            longitude
+	 */
+	public setLongitude(longitude: number): void {
+		super.x = longitude;
+	}
+
+	/**
+	 * Get the latitude
+	 * 
+	 * @return latitude
+	 */
+	public getLatitude(): number {
+		return super.y;
+	}
+
+	/**
+	 * Set the latitude
+	 * 
+	 * @param latitude
+	 *            latitude
+	 */
+	public setLatitude(latitude: number): void {
+		super.y = latitude;
+	}
+
+	/**
+	 * Get the unit
+	 * 
+	 * @return unit
+	 */
+	public getUnit(): Unit | undefined {
+		return this.unit;
+	}
+
+	/**
+	 * Set the unit
+	 * 
+	 * @param unit
+	 *            unit
+	 */
+	public setUnit(unit?: Unit): void {
+		this.unit = unit;
+	}
+
+	/**
+	 * Is in the provided unit type
+	 * 
+	 * @param unit
+	 *            unit
+	 * @return true if in the unit
+	 */
+	public isUnit(unit?: Unit): boolean {
+		return this.unit == unit;
+	}
+
+	/**
+	 * Is this point in degrees
+	 * 
+	 * @return true if degrees
+	 */
+	public isDegrees(): boolean {
+		return this.isUnit(Unit.DEGREE);
+	}
+
+	/**
+	 * Is this point in meters
+	 * 
+	 * @return true if meters
+	 */
+	public isMeters(): boolean {
+		return this.isUnit(Unit.METER);
+	}
+
+	/**
+	 * Convert to the unit
+	 * 
+	 * @param unit
+	 *            unit
+	 * @return point in units, same point if equal units
+	 */
+	public toUnit(unit: Unit): Point {
+		let newPoint: Point;
+		if (this.isUnit(unit)) {
+			newPoint = this;
+		} else {
+			newPoint = GridUtils.toUnit(this.unit, this.getLongitude(), this.getLatitude(),
+				unit);
+		}
+		return newPoint;
+	}
+
+	/**
+	 * Convert to degrees
+	 * 
+	 * @return point in degrees, same point if already in degrees
+	 */
+	public toDegrees(): Point {
+		return this.toUnit(Unit.DEGREE);
+	}
+
+	/**
+	 * Convert to meters
+	 * 
+	 * @return point in meters, same point if already in meters
+	 */
+	public toMeters(): Point {
+		return this.toUnit(Unit.METER);
+	}
+
+	/**
+	 * Get the pixel where the point fits into tile
+	 * 
+	 * @param tile
+	 *            tile
+	 * @return pixel
+	 */
+	public getPixel(tile: GridTile): Pixel {
+		return this.getPixel(tile.getWidth(), tile.getHeight(), tile.getBounds());
+	}
+
+	/**
+	 * Get the pixel where the point fits into the bounds
+	 * 
+	 * @param width
+	 *            width
+	 * @param height
+	 *            height
+	 * @param bounds
+	 *            bounds
+	 * @return pixel
+	 */
+	public getPixel(width: number, height: number, bounds: Bounds): Pixel {
+		return GridUtils.getPixel(width, height, bounds, this);
+	}
+
+	/**
+	 * Copy the point
+	 * 
+	 * @return point copy
+	 */
+	public copy(): Point {
+		return new Point(this);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public equals(other: Point): boolean {
+		if (this == other)
+			return true;
+		if (!super.equals(other))
+			return false;
+		if (!(other instanceof Point))
+			return false;
+		
+		if (this.unit != other.getUnit())
+			return false;
+		return true;
+	}
+
+}
