9cf3502854b0604dbb9203af3fbf162343459c77
Kevin Gilland <kgilland@caci.com>
2022-09-20 04:31:30 -0600

    chore: Port of PropertyConstants from grid-java

---
+++ lib/property/PropertyConstants.ts
@@ -0,0 +1,73 @@
+/**
+ * Grid property constants
+ *
+ * @author osbornb
+ */
+export class PropertyConstants {
+
+	/**
+	 * Property part divider
+	 */
+	public static readonly PROPERTY_DIVIDER = ".";
+
+	/**
+	 * Grid
+	 */
+	public static readonly GRID = "grid";
+
+	/**
+	 * Labeler
+	 */
+	public static readonly LABELER = "labeler";
+
+	/**
+	 * Grids
+	 */
+	public static readonly GRIDS = "grids";
+
+	/**
+	 * Propagate
+	 */
+	public static readonly PROPAGATE = "propagate";
+
+	/**
+	 * Width
+	 */
+	public static readonly WIDTH = "width";
+
+	/**
+	 * Text Size
+	 */
+	public static readonly TEXT_SIZE = "text_size";
+
+	/**
+	 * Buffer
+	 */
+	public static readonly BUFFER = "buffer";
+
+	/**
+	 * Enabled
+	 */
+	public static readonly ENABLED = "enabled";
+
+	/**
+	 * Min Zoom
+	 */
+	public static readonly MIN_ZOOM = "min_zoom";
+
+	/**
+	 * Max Zoom
+	 */
+	public static readonly MAX_ZOOM = "max_zoom";
+
+	/**
+	 * Color
+	 */
+	public static readonly COLOR = "color";
+
+	/**
+	 * Lines
+	 */
+	public static readonly LINES = "lines";
+
+}
